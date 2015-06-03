8b05e2f8a261372d8027baa4079c688c3b818332
Nastya Stavitskaya <nastycorps@gmail.com>
2015-06-03 20:18:55 +0200

    Deleted unnecessary files

--- db/migrate/20150603131716_create_cards.rb
+++ db/migrate/20150603131716_create_cards.rb
@@ -2,7 +2,7 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.text :original_text, :translated_text
-      t.date :review_date
+      t.datetime :review_date
 
       t.timestamps 
     end
