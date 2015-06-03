18b93a94b71fdff17a43bf8fd6aee84b44eb6298
Nastya Stavitskaya <nastycorps@gmail.com>
2015-06-03 16:44:08 +0200

    Modified all cards page

--- db/migrate/20150603131716_create_cards.rb
+++ db/migrate/20150603131716_create_cards.rb
@@ -1,8 +1,10 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
+      t.text :original_text, :translated_text
+      t.date :review_date
 
-      t.timestamps null: false
+      t.timestamps 
     end
   end
 end
