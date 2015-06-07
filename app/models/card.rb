73ae59507225783819a767d06e0f302b84e666ca
Nastya Stavitskaya <nastycorps@gmail.com>
2015-06-07 14:12:23 +0200

    Added simple form and review_date

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,17 @@
 class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, presence: true, uniqueness: true
-  validate :sametexts
+  validates :original_text, :translated_text, presence: true
+  validate :same_texts
+  before_save :add_days, on: :create
+  scope :check_review_date, -> { where("review_date <=?", Date.today) }
   
-  def sametexts
-    if self.original_text == self.translated_text
-      errors.add(:Error!, "Original text and translation can't be same!")
+  def same_texts
+    if original_text.mb_chars.downcase.to_s == translated_text.mb_chars.downcase.to_s
+      errors.add(:original_text, "and #{:translated_text} can't be same!")
     end
   end
+
+  def add_days
+    self.review_date = review_date + 3.days
+  end
+
 end
