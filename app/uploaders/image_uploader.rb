606b2b6d8b4e115a4811273eed0abd77bc9a47af
Nastya Stavitskaya <nastycorps@gmail.com>
2015-09-28 16:00:51 -0700

    Added public/uploads to gitignore

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
@@ -7,8 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+    storage :file
+   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -33,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-     process :resize_to_fit => [360, 360]
+    process resize_to_fit: [360, 360]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
