e7134b312488a7c6fff5c237296fd48af1a61fda
Nastya Stavitskaya <nastycorps@gmail.com>
2015-09-29 17:22:12 -0700

    Added AWS to app

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,13 +5,13 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-    storage :file
-   # storage :fog
+    # storage :file
+    storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "myflashcardsawsbucket/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [360, 360]
+      process resize_to_fit: [360, 360]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
