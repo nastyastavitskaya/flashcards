086945483c274ee0d0d140e8899a8d95cbf33c2e
Nastya Stavitskaya <nastycorps@gmail.com>
2015-12-11 11:41:33 -0800

    Changed oauth controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,10 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, except: [:destroy]
 
   def new
-     @user = User.new
   end
 
   def create
-    if @user = login(params[:email], params[:password], params[:remember_me])
+    if login(params[:email], params[:password], params[:remember_me])
       flash[:success] = "Welcome back!"
       redirect_back_or_to root_path
     else
